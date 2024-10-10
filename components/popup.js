8f1e165533357b9500ca46a9470221f88535d01b
inrain9953 <inrainconstruction46@gmail.com>
2024-10-10 10:47:57 +0530

    ICPL Final Modification

--- components/popup.js
+++ components/popup.js
@@ -5,14 +5,14 @@ import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 const PopUp = () => {
   return (
     <div>
-        <div className='fixed bottom-10 right-7 text-xl z-50'>
-            <a href='tel:+919910220794' className='bg-sky-500 p-6 rounded-full'>
-                <CallIcon className='cursor-pointer' />
+        <div className='fixed bottom-16 md:bottom-20 right-7 md:right-10 text-xl z-50'>
+            <a href='tel:+919910220794' className='bg-sky-500 md:p-8 p-6 rounded-full'>
+                <CallIcon className='cursor-pointer' fontSize='medium' />
             </a>
         </div>
-        <div className='fixed bottom-10 left-7 text-xl z-50'>
-            <a href='https://wa.me/919910220794' className='bg-green-500 p-6 rounded-full'>
-                <WhatsAppIcon className='cursor-pointer' />
+        <div className='fixed bottom-16 md:bottom-20 left-7 md:left-10 text-xl z-50'>
+            <a href='https://wa.me/919910220794' className='bg-green-500 md:p-8 p-6 rounded-full'>
+                <WhatsAppIcon className='cursor-pointer' fontSize='medium' />
             </a>
         </div>
     </div>
